88491707651a482d9147658387feedc5332fa509
Mike Taves <mwtoews@gmail.com>
2019-05-21 17:33:20 +1200

    Test more complex shapefile example

---
+++ tests/test_shp.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import geopandas
+import pytest
+import os
+try:
+    import rtree
+except ImportError:
+    rtree = False
+
+import sys
+sys.path.insert(
+    0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+import swn
+
+datadir = os.path.join('tests', 'data')
+
+
+@pytest.fixture
+def dn():
+    shp_srs = os.path.join(datadir, 'DN2_Coastal_strahler1z_stream_vf.shp')
+    lines = geopandas.read_file(shp_srs)
+    lines.set_index('nzsegment', inplace=True)
+    return swn.SurfaceWaterNetwork(lines)
+
+
+def test_init(dn):
+    assert len(dn) == 304
+
+
+def test_process(dn):
+    dn.process()
+    assert dn.END_NODE == 0
+    if rtree:
+        assert dn.lines_idx is not None
+    else:
+        assert dn.lines_idx is None
+    assert dn.to_node.index is dn.lines.index
+    # outlets
+    assert dn.to_node[dn.to_node == 0].index.values.tolist() == \
+        [3046700, 3046737, 3046736]
